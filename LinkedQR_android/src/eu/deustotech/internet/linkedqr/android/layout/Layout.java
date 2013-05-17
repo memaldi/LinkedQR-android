fc52cb9c3e077a4011a8da6ff3e8e2b77fc364ed
Mikel Emaldi <m.emaldi@gmail.com>
2013-05-17 17:20:15 +0200

    Layouts loaded dynamically.

---
+++ LinkedQR_android/src/eu/deustotech/internet/linkedqr/android/layout/Layout.java
@@ -0,0 +1,8 @@
+package eu.deustotech.internet.linkedqr.android.layout;
+
+/**
+ * Created by mikel on 17/05/13.
+ */
+public interface Layout {
+    public int getLayout();
+}
---
+++ LinkedQR_android/src/eu/deustotech/internet/linkedqr/android/layout/Person.java
@@ -0,0 +1,13 @@
+package eu.deustotech.internet.linkedqr.android.layout;
+
+import eu.deustotech.internet.linkedqr.android.R;
+
+/**
+ * Created by mikel on 17/05/13.
+ */
+public class Person implements Layout {
+    @Override
+    public int getLayout() {
+        return R.layout.person;
+    }
+}
